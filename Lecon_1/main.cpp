72235e4137a97e5fe9c233f60c6e414e9fb3bd88
Ginkoyen <pierreledu1@free.fr>
2015-06-29 07:51:01 +0200

    Update 1 OpenClassRoom lecon (main.cpp, Lecon_1.cbp) and add a SceneOpenGL.cpp and .h

--- Lecon_1/main.cpp
+++ Lecon_1/main.cpp
@@ -1,58 +1,19 @@
-#include <SDL2/SDL.h>
-#include <iostream>
+#include "SceneOpenGL.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    // Variables
+    // Création de la sène
+    SceneOpenGL scene("Chapitre 3", 800, 600);
 
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Event event;
-    bool end = false;
+    // Initialisation de la scène
+    if(scene.initialiserFenetre() == false)
+	return -1;
 
+	if(scene.initGL() == false)
+	return -1;
 
-    // Window
-
-    window = SDL_CreateWindow("SDL 2.0 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-
-    if(!window)
-    {
-        std::cout << "SDL Error : " << SDL_GetError() << std::endl;
-        return -1;
-    }
-
-
-    // Renderer
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-
-    if(!renderer)
-    {
-        std::cout << "SDL Error : " << SDL_GetError() << std::endl;
-        return -1;
-    }
-
-
-    // Event loop
-
-    while(!end)
-    {
-        SDL_WaitEvent(&event);
-
-        if(event.window.event == SDL_WINDOWEVENT_CLOSE)
-            end = true;
-
-
-        // Drawing
-
-        SDL_SetRenderDrawColor(renderer, 0x00, 0xAA, 0xAA, 0xFF);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
-    }
+    // Boucle Principale
+    scene.bouclePrincipale();
 
     return 0;
 }
