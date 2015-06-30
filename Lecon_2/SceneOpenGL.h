902a5eb1d66a8c2c2edd216a793e076684b0ed51
Ginkoyen <pierreledu1@free.fr>
2015-06-30 05:42:58 +0200

    Add a 2 OpenClassRoom lecon, add a shaders and use a color

---
+++ Lecon_2/SceneOpenGL.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <string>
+#include "Shaders/Shader.h"
+
+class SceneOpenGL
+{
+    public:
+
+    SceneOpenGL(std::string titreFenetre, int largeurFenetre, int hauteurFenetre);
+    ~SceneOpenGL();
+
+    bool initialiserFenetre();
+    bool initGL();
+    void bouclePrincipale();
+
+    private:
+
+    std::string m_titreFenetre;
+    int m_largeurFenetre;
+    int m_hauteurFenetre;
+
+    SDL_Window* m_fenetre;
+    SDL_GLContext m_contexteOpenGL;
+    SDL_Event m_evenements;
+};
